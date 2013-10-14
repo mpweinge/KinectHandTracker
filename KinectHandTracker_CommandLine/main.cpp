3b7d0b5cbe66ec7cf1ba5cd336d5d7c14ad748d6
Michael Weingert <mpweinge@uwaterloo.ca>
2013-10-14 12:50:03 -0400

    Initial Commit

---
+++ KinectHandTracker_CommandLine/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  KinectHandTracker_CommandLine
+//
+//  Created by Michael Weingert on 2013-10-14.
+//  Copyright (c) 2013 Michael Weingert. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
