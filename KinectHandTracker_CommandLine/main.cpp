d4e78a9dca12e9fda6b2a9c633957b5044f6b14d
Michael Weingert <mpweinge@uwaterloo.ca>
2013-10-14 23:44:55 -0400

    Initial Commit

--- KinectHandTracker_CommandLine/main.cpp
+++ KinectHandTracker_CommandLine/main.cpp
@@ -1,18 +1,245 @@
-//
-//  main.cpp
-//  KinectHandTracker_CommandLine
-//
-//  Created by Michael Weingert on 2013-10-14.
-//  Copyright (c) 2013 Michael Weingert. All rights reserved.
-//
+/****************************************************************************
+ *                                                                           *
+ *  OpenNI 1.x Alpha                                                         *
+ *  Copyright (C) 2011 PrimeSense Ltd.                                       *
+ *                                                                           *
+ *  This file is part of OpenNI.                                             *
+ *                                                                           *
+ *  OpenNI is free software: you can redistribute it and/or modify           *
+ *  it under the terms of the GNU Lesser General Public License as published *
+ *  by the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                      *
+ *                                                                           *
+ *  OpenNI is distributed in the hope that it will be useful,                *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the             *
+ *  GNU Lesser General Public License for more details.                      *
+ *                                                                           *
+ *  You should have received a copy of the GNU Lesser General Public License *
+ *  along with OpenNI. If not, see <http://www.gnu.org/licenses/>.           *
+ *                                                                           *
+ ****************************************************************************/
+//---------------------------------------------------------------------------
+// Includes
+//---------------------------------------------------------------------------
+#include "NiHandViewer.h"
 
-#include <iostream>
 
-int main(int argc, const char * argv[])
+//---------------------------------------------------------------------------
+// Defines
+//---------------------------------------------------------------------------
+#define SAMPLE_XML_PATH "../../Config/SamplesConfig.xml"
+
+
+//---------------------------------------------------------------------------
+// Globals
+//---------------------------------------------------------------------------
+xn::Context		g_context;
+xn::ScriptNode	g_scriptNode;
+xn::DepthGenerator g_DepthGenerator;
+xn::UserGenerator g_UserGenerator;
+
+XnBool g_bNeedPose = FALSE;
+XnChar g_strPose[20] = "";
+
+#define CHECK_RC(nRetVal, what)					    \
+if (nRetVal != XN_STATUS_OK)				    \
+{								    \
+printf("%s failed: %s\n", what, xnGetStatusString(nRetVal));    \
+return nRetVal;						    \
+}
+
+void XN_CALLBACK_TYPE UserCalibration_CalibrationComplete(xn::SkeletonCapability& /*capability*/, XnUserID nId, XnCalibrationStatus eStatus, void* /*pCookie*/)
 {
+    XnUInt32 epochTime = 0;
+    xnOSGetEpochTime(&epochTime);
+    if (eStatus == XN_CALIBRATION_STATUS_OK)
+    {
+        // Calibration succeeded
+        printf("%d Calibration complete, start tracking user %d\n", epochTime, nId);
+        g_UserGenerator.GetSkeletonCap().StartTracking(nId);
+    }
+    else
+    {
+        // Calibration failed
+        printf("%d Calibration failed for user %d\n", epochTime, nId);
+        if(eStatus==XN_CALIBRATION_STATUS_MANUAL_ABORT)
+        {
+            printf("Manual abort occured, stop attempting to calibrate!");
+            return;
+        }
+        if (g_bNeedPose)
+        {
+            g_UserGenerator.GetPoseDetectionCap().StartPoseDetection(g_strPose, nId);
+        }
+        else
+        {
+            g_UserGenerator.GetSkeletonCap().RequestCalibration(nId, TRUE);
+        }
+    }
+}
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+// Callback: New user was detected
+void XN_CALLBACK_TYPE User_NewUser(xn::UserGenerator& /*generator*/, XnUserID nId, void* /*pCookie*/)
+{
+    XnUInt32 epochTime = 0;
+    xnOSGetEpochTime(&epochTime);
+    printf("%d New User %d\n", epochTime, nId);
+    // New user found
+    if (g_bNeedPose)
+    {
+        g_UserGenerator.GetPoseDetectionCap().StartPoseDetection(g_strPose, nId);
+    }
+    else
+    {
+        g_UserGenerator.GetSkeletonCap().RequestCalibration(nId, TRUE);
+    }
+}
+// Callback: An existing user was lost
+void XN_CALLBACK_TYPE User_LostUser(xn::UserGenerator& /*generator*/, XnUserID nId, void* /*pCookie*/)
+{
+    XnUInt32 epochTime = 0;
+    xnOSGetEpochTime(&epochTime);
+    printf("%d Lost user %d\n", epochTime, nId);
+}
+// Callback: Detected a pose
+void XN_CALLBACK_TYPE UserPose_PoseDetected(xn::PoseDetectionCapability& /*capability*/, const XnChar* strPose, XnUserID nId, void* /*pCookie*/)
+{
+    XnUInt32 epochTime = 0;
+    xnOSGetEpochTime(&epochTime);
+    printf("%d Pose %s detected for user %d\n", epochTime, strPose, nId);
+    g_UserGenerator.GetPoseDetectionCap().StopPoseDetection(nId);
+    g_UserGenerator.GetSkeletonCap().RequestCalibration(nId, TRUE);
+}
+// Callback: Started calibration
+void XN_CALLBACK_TYPE UserCalibration_CalibrationStart(xn::SkeletonCapability& /*capability*/, XnUserID nId, void* /*pCookie*/)
+{
+    XnUInt32 epochTime = 0;
+    xnOSGetEpochTime(&epochTime);
+    printf("%d Calibration started for user %d\n", epochTime, nId);
+}
+
+int main(int argc, char* argv[])
+{
+	XnStatus				rc;
+	xn::EnumerationErrors	errors;
+    
+	// Create a context with default settings
+	rc = g_context.InitFromXmlFile("/Users/Mikey/KinectHandTracker_CommandLine/KinectHandTracker_CommandLine/SamplesConfig.xml", g_scriptNode, &errors);
+	if (rc == XN_STATUS_NO_NODE_PRESENT)
+	{
+		XnChar strError[1024];
+		errors.ToString(strError, 1024);
+		printf("%s\n", strError);
+		return (rc);
+	}
+	else if (rc != XN_STATUS_OK)
+	{
+		printf("Open failed: %s\n", xnGetStatusString(rc));
+		return (rc);
+	}
+    
+    rc = g_context.FindExistingNode(XN_NODE_TYPE_DEPTH, g_DepthGenerator);
+    CHECK_RC(rc,"No depth");
+    
+    rc = g_context.FindExistingNode(XN_NODE_TYPE_USER, g_UserGenerator);
+    if (rc != XN_STATUS_OK)
+    {
+        rc = g_UserGenerator.Create(g_context);
+        CHECK_RC(rc, "Find user generator");
+    }
+    
+    XnCallbackHandle hUserCallbacks, hCalibrationStart, hCalibrationComplete, hPoseDetected;
+    if (!g_UserGenerator.IsCapabilitySupported(XN_CAPABILITY_SKELETON))
+    {
+        printf("Supplied user generator doesn't support skeleton\n");
+        return 1;
+    }
+    
+    rc = g_UserGenerator.RegisterUserCallbacks(User_NewUser, User_LostUser, NULL, hUserCallbacks);
+    CHECK_RC(rc, "Register to user callbacks");
+    rc = g_UserGenerator.GetSkeletonCap().RegisterToCalibrationStart(UserCalibration_CalibrationStart, NULL, hCalibrationStart);
+    CHECK_RC(rc, "Register to calibration start");
+    rc = g_UserGenerator.GetSkeletonCap().RegisterToCalibrationComplete(UserCalibration_CalibrationComplete, NULL, hCalibrationComplete);
+    CHECK_RC(rc, "Register to calibration complete");
+    
+    if (g_UserGenerator.GetSkeletonCap().NeedPoseForCalibration())
+    {
+        g_bNeedPose = TRUE;
+        if (!g_UserGenerator.IsCapabilitySupported(XN_CAPABILITY_POSE_DETECTION))
+        {
+            printf("Pose required, but not supported\n");
+            return 1;
+        }
+        rc = g_UserGenerator.GetPoseDetectionCap().RegisterToPoseDetected(UserPose_PoseDetected, NULL, hPoseDetected);
+        CHECK_RC(rc, "Register to Pose Detected");
+        g_UserGenerator.GetSkeletonCap().GetCalibrationPose(g_strPose);
+    }
+    
+    g_UserGenerator.GetSkeletonCap().SetSkeletonProfile(XN_SKEL_PROFILE_ALL);
+    
+    rc = g_context.StartGeneratingAll();
+    CHECK_RC(rc, "StartGenerating");
+    
+    XnUserID aUsers[15];
+    XnUInt16 nUsers;
+    XnSkeletonJointTransformation leftHand;
+    XnSkeletonJointTransformation rightHand;
+    
+    printf("Starting to run\n");
+    if(g_bNeedPose)
+    {
+        printf("Assume calibration pose\n");
+    }
+    
+    bool bTrackedFirstHand = false;
+	while ((!xnOSWasKeyboardHit()) && (!bTrackedFirstHand))
+    {
+        g_context.WaitOneUpdateAll(g_UserGenerator);
+        // print the torso information for the first user already tracking
+        nUsers=15;
+        g_UserGenerator.GetUsers(aUsers, nUsers);
+        for(XnUInt16 i=0; i<nUsers; i++)
+        {
+            if(g_UserGenerator.GetSkeletonCap().IsTracking(aUsers[i])==FALSE)
+                continue;
+            
+            g_UserGenerator.GetSkeletonCap().GetSkeletonJoint(aUsers[i],XN_SKEL_LEFT_HAND,leftHand);
+            printf("user %d: left hand at (%6.2f,%6.2f,%6.2f)\n",aUsers[i],
+                   leftHand.position.position.X,
+                   leftHand.position.position.Y,
+                   leftHand.position.position.Z);
+            g_UserGenerator.GetSkeletonCap().GetSkeletonJoint(aUsers[i],XN_SKEL_RIGHT_HAND,rightHand
+                                                              );
+            printf("user %d: right hand at (%6.2f,%6.2f,%6.2f)\n",aUsers[i],
+                   rightHand.position.position.X,
+                   rightHand.position.position.Y,
+                   rightHand.position.position.Z);
+            
+            bTrackedFirstHand = true;
+            break;
+        }
+        
+    }
+    
+	SimpleViewer& viewer = HandViewer::CreateInstance(g_context);
+    
+    viewer.setHandStartingLocations(leftHand, rightHand);
+	rc = viewer.Init(argc, argv);
+	if (rc != XN_STATUS_OK)
+	{
+		printf("Viewer init failed: %s\n", xnGetStatusString(rc));
+		return 1;
+	}
+    
+	rc = viewer.Run();
+	if (rc != XN_STATUS_OK)
+	{
+		printf("Viewer run failed: %s\n", xnGetStatusString(rc));
+		return 1;
+	}
+    
+	return 0;
+    
 }
 
